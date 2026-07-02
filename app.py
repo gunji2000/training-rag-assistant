53e1e7c6b474c06293a36635fd78c937b88bad87
gunji2000 <keina.gunji@rakus-partners.co.jp>
2026-07-02 10:23:53 +0900

    feat: Gemini APIを表示できるか確認

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-import chainlit as cl
+from dotenv import load_dotenv
+import os
 
-@cl.on_message
-async def main(message: cl.Message):
-    await cl.Message(
-        content=f"受け取ったよ: {message.content}"
-    ).send()
+load_dotenv()
+
+# .envに記載したAPIキーを読み込む
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# APIキーを表示
+print(api_key)
+
+# import chainlit as cl
+
+# @cl.on_message
+# async def main(message: cl.Message):
+#     await cl.Message(
+#         content=f"受け取ったよ: {message.content}"
+#     ).send()
